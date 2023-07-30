ba82331ce54b73834bae9d28c2223fe6de88a089
TheWisePigeon <zozozozeph@gmail.com>
2023-07-30 18:58:18 +0000

    minor updates

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
 
 import { migrate } from "./commands/migrate.js";
-import { connect } from "./commands/connect.js";
+// import { connect } from "./commands/connect.js";
 import { command_is_valid } from "./utils.js";
 import * as dotenv from "dotenv"
 
 dotenv.config()
 
-const [_,__, command, ...args] = process.argv
+const [_,__, command, ___] = process.argv
 
 if (
   command_is_valid(command)
@@ -15,16 +15,16 @@ if (
   switch (command) {
     case "migrate": {
       await migrate()
-      process.exit()
+      process.exit(0)
     }
-    case "init":{
-
-    }
-    case "connect":{
-      await connect()
-      process.exit()
-    }
+    // case "init":{
+    //
+    // }
+    // case "connect":{
+    //   await connect()
+    //   process.exit()
+    // }
   }
 }
 console.log(`Invalid command ${command}`)
-process.exit()
+process.exit(1)
--- utils.js
+++ utils.js
@@ -2,8 +2,8 @@ const available_commands = [
   "migrate",
   "init",
   "connect"
-] 
+]
 
-export function command_is_valid( command) {
+export function command_is_valid(command) {
   return available_commands.includes(command)
 }
