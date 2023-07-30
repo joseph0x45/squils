cad29aa7b2e435a1613c55926e7c61be5337efa0
TheWisePigeon <zozozozeph@gmail.com>
2023-07-30 18:55:34 +0000

    feat: migration command

--- commands/migrate.js
+++ commands/migrate.js
@@ -1,25 +1,37 @@
-import { existsSync, readFileSync, readdirSync } from "fs"
+import { existsSync, readdirSync } from "fs"
+import postgres from "postgres"
 
 export async function migrate() {
   try {
     if (existsSync('./migrations')) {
-      const files = readdirSync('./migrations')
+      const files = readdirSync('./migrations').filter(file_name => file_name.endsWith('.sql'))
+      if (files.length === 0) {
+        console.log('squils did not find any sql file in the migrations folder. Aborting operation....')
+        process.exit(0)
+      }
       const db_url = process.env.DB_URL ?? ""
-      if(db_url===""){
+      if (db_url === "") {
         console.log('Database URI not found in environment')
         console.log('Export the DB_URL in the local environment or use a .env file at the root of the folder where you are running squils')
-        return
+        process.exit(1)
       }
-      files.map(file_name => {
-        if (file_name.endsWith('.sql')) {
-          const raw_sql = readFileSync(`./migrations/${file_name}`, 'utf8')
-          console.log(raw_sql)
-        }
-      })
-      return
+      for (let i = 0; i < files.length; i++) {
+        const file_name = files[i];
+        const sql = postgres(db_url)
+        console.log(`Running transaction ${file_name.substring(file_name.indexOf('_') + 1)}...`)
+        await sql.begin(async sql => {
+          await sql.file(`./migrations/${file_name}`)
+        })
+        console.log(`Done`)
+      }
+      console.log('All transactions have been run successfuly')
+      process.exit(0)
     }
     console.log('Can not find `migrations` folder')
+    process.exit(1)
   } catch (err) {
-
+    console.log(err)
+    console.log(`Report bugs at https://github.com/TheWisePigeon/squils/issues`)
+    process.exit(1)
   }
 }
